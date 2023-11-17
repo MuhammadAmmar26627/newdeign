8fd88bf8ddf8d31be96970e0d642bbe1ec0eec5b
Muhammad Ammar <82372110+MuhammadAmmar26627@users.noreply.github.com>
2023-11-17 20:58:22 +0500

    Add files via upload

--- App.py
+++ App.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import os
 import pandas as pd
-st.set_page_config(layout="wide")
+from function_file import *
+# st.set_page_config(layout="wide")
+with open ('custom.css') as f:
+			st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+if "material_df" not in st.session_state:
+    st.session_state["material_df"]=pd.read_csv("Material.csv")
+if "lab_df" not in st.session_state:
+    st.session_state["lab_df"]=pd.read_csv("Labour.csv")
 if "df" not in st.session_state:
   if os.path.exists("Rate.csv"):
       st.session_state["df"]=pd.read_csv("Rate.csv")
@@ -9,6 +17,33 @@ if "df" not in st.session_state:
       st.session_state["df"]=pd.read_csv("rate_.csv")
       st.session_state["df"].to_csv("Rate.csv",index=False)
 df=st.session_state["df"]
+lab_df=st.session_state["lab_df"]
+#####################################
+col1,col2,col3=st.sidebar.columns(3)
+  
+rigid=col1.checkbox("Rigid")
+
+custom=col2.checkbox("Custom_S")
+custom_p = col3.checkbox('Custom_P')
+#####################################
+# with st.sidebar.form("my_form"):
+#     ######## Client Data ###########
+#     st.header("Packaging Estimation Sheet")
+#     st.header("Client Bio")
+#     col1,col2=st.columns(2)
+#     client_name=col1._text_input("Client Name")
+#     CSR=col2._text_input("CSR Name")
+#     col1,col2=st.columns(2)
+#     client_email=col1._text_input("Client Email")
+#     Phone=col2._text_input("Phone Number")
+
+#     # checkbox_val = st.checkbox("Form checkbox")
+
+#     # Every form must have a submit button.
+#     submitted = st.form_submit_button("Submit")
+#     if submitted:
+#           st.write("slider", 0, "checkbox", rigid)
+#####################################
 ######## Client Data ###########
 # form=st.sidebar.form("Calculate Material and Labour")
 
@@ -24,7 +59,7 @@ Phone=col2._text_input("Phone Number")
 
 
 ########### Sheet Data (Size) #############
-rigid=st.sidebar.checkbox("Rigid")
+
 st.sidebar.header("Sheet Size")
 if rigid:
     col1, col2 = st.sidebar.columns(2)
@@ -55,59 +90,61 @@ gsm_list=[210,230,250,270,300,350,420,460,500,540,600,700,]
 if Material=="Art Paper":
     gsm_list=[128,150,]
 gsm = col2.selectbox("GSM",gsm_list)
+if custom:
+    col1, col2 = st.sidebar.columns(2)
+    W_S=col1.number_input("W_Sheet", min_value=0.0)
+    L_S=col2.number_input("L_Sheet", min_value=0.0)
+else:
+    col1, col2 = st.sidebar.columns(2)
+    Sheet_size=["23x36","25x36","22x28",'20x30','25x30',"27x34"]
+    Sheet = col1.selectbox( 
+        "Sheet Size",
+        Sheet_size)
+    Packets_size=[("9x7.66","11.5x7.2","12x7.66","9x11.5","11.5x12","12x23","18x23"),
+    ("9x8.33","12.5x7.5","12x8.33","9x12.5","12.5x12","12x25","18x25"),
+    ('9.33x7.33',"11x14","14x22"),("10x7.5","10x15",'10x20','15x20'),
+    ("10x8.33","10x12.5",'12.5x15','15x25'),('9x11.33','13.5x17','17x27')]
 
-col1, col2 = st.sidebar.columns(2)
-Sheet_size=["23x36","25x36","22x28",'20x30','25x30',"27x34"]
-Sheet = col1.selectbox( 
-    "Sheet Size",
-    Sheet_size
-)
-Packets_size=[("9x7.66","11.5x7.2","12x7.66","9x11.5","11.5x12","12x23","18x23"),
-  ("9x8.33","12.5x7.5","12x8.33","9x12.5","12.5x12","12x25","18x25"),
-  ('9.33x7.33',"11x14","14x22"),("10x7.5","10x15",'10x20','15x20'),
-  ("10x8.33","10x12.5",'12.5x15','15x25'),('9x11.33','13.5x17','17x27')]
+    packet_index=Sheet_size.index(Sheet)
+    Packet = col2.selectbox(
+        "Packet Size",
+        Packets_size[packet_index])
+    pack=Packet.split("x")
+    W_S,L_S=float(pack[0]),float(pack[1])
+if custom_p:
+    col1, col2 = st.sidebar.columns(2)
+    W_P=col1.number_input("W_Print", min_value=0.0)
+    L_P=col2.number_input("L_Print", min_value=0.0)
+else:
+    W_P=W_S
+    L_P=L_S
 
-packet_index=Sheet_size.index(Sheet)
-Packet = col2.selectbox(
-    "Packet Size",
-    Packets_size[packet_index]
-)
 col1, col2 = st.sidebar.columns(2)
 up = col1.number_input("Box Uping", min_value=1)
 Req_Q = col2.number_input("Required Quantity", min_value=1)
 
-custom=st.sidebar.checkbox("Custom Size")
-if custom:
-    col1, col2 = st.sidebar.columns(2)
-    W_S=col1.number_input("W_Sheet", min_value=0.0)
-
-    # st.write(W_S)
-
-    L_S=col2.number_input("L_Sheet", min_value=0.0)
-# st.sidebar.header("Material")
 
 
 
 
 
-agree = st.sidebar.checkbox('Print Size Same as Sheet Size')
-col1, col2 = st.sidebar.columns(2)
-W_P=col1.number_input("W_Print", min_value=0.0)
-L_P=col2.number_input("L_Print", min_value=0.0)
+
+
 # submitted = st.sidebar.form_submit_button("Submit")
 ################################################
 
 st.sidebar.header("Corrugation")
 col1, col2 = st.sidebar.columns(2)
-stock = col1.selectbox(
+pasting = col1.selectbox(
+    "Corrugation Pasting",
+    ["None","Single Side", "Double Side",]
+)
+stock = col2.selectbox(
     "Corrugation Material",
     ["L1", "E Flute", "B Flute"]
 )
 
-pasting = col2.selectbox(
-    "Corrugation Pasting",
-    ["None","Single Side", "Double Side",]
-)
+
 
 st.sidebar.header("Printing Colors")
 col1, col2, col3 = st.sidebar.columns(3)
@@ -123,7 +160,7 @@ st.sidebar.header("Add-Ons")
 col1, col2, col3 = st.sidebar.columns(3)
 Foil = col1.selectbox(
     "Foiling",
-    ["None","Cupper","Brass","Zinc"],
+    ["None","Copper","Brass","Zinc"],
      index=0
 )
 f_l=col2.number_input("Foiling_L", min_value=0.0)
@@ -167,10 +204,10 @@ st.sidebar.header("Lamination")
 col1,col2=st.sidebar.columns(2)
 inside=col1.selectbox(
     "Inside Lamination",
-     ["None","Matte","Gloss","Soft Touch","Varnish",])
+     ["None","Matte","Gloss","Soft Touch","Aqueous Coating",])
 outside=col2.selectbox(
     "Outside Lamination",
-     ["None","Matte","Gloss","Soft Touch","Varnish",])
+     ["None","Matte","Gloss","Soft Touch","Aqueous Coating",])
 
 ############ Additional Expense ##########
 
@@ -178,15 +215,83 @@ st.sidebar.header("Additional Expense")
 col1, col2 = st.sidebar.columns(2)
 Mics = col1.number_input("Micsellneus", min_value=0)
 Profit_margin = col2.number_input("Profit Margin", min_value=0)
-# project_difficulty = col3.number_input("Project Difficulty", min_value=0)
+submitted = st.sidebar.button("Submit")
 
 
-### disply Machine size
-# submitted = st.sidebar.form_submit_button("Submit")
+############ Calculation  #####################
+if submitted:
+    machine_rate=find_machine_size(W_S,L_S,st.session_state["df"])
+    Sheets=Req_Q/up
+    print_sheet=Print_Sheet_calculator(Sheets)
+    laminate_sheet=Lamination_sheets_calculator(Sheets)
+    process_color_rate,pantone_color_rate,matallic_color_rate=Printing_Calculator(machine_rate,process_color,pantone_color,matallic_color,print_sheet)
+    lamination_price=Lamination_price_calculator(W_P,L_P,laminate_sheet,inside,outside,machine_rate)
+    die_cut_price=Die_cut_price(print_sheet,machine_rate)
+    pasting_material=Pasting_Calculator(machine_rate,Req_Q)
+    UV_coating=UV_price(Uv_l,Uv_w,print_sheet,machine_rate)
+    foiling=foil_price(f_l,f_w,Foil,laminate_sheet,machine_rate)
+    Debosing=debosing_price(d_l,d_l,machine_rate,print_sheet)
+    Embosing=embosing_price(E_l,E_w,machine_rate,print_sheet)
+    carrug_lab=corgation_price(W_S,L_S,pasting,laminate_sheet,machine_rate)
+    st.dataframe(machine_rate,hide_index=True)
 
-# if submitted:
-#     if agree:
-#         W_P=W_S
-#         L_P=L_S
-#     else:
-#         pass
+    ################### Table Update ################
+    # print(st.session_state["lab_df"].columns)
+    lab_df.set_index('index',inplace=True)
+    # st.write(st.session_state["lab_df"].loc["Lam"])
+    lab_df.loc["Printing"]=(process_color_rate,pantone_color_rate,matallic_color_rate,process_color_rate+pantone_color_rate+matallic_color_rate)
+    lab_df.loc["Lam"]=(lamination_price[0],lamination_price[1],0,lamination_price[0]+lamination_price[1])
+    lab_df.loc["Die cut"]=(die_cut_price,0,0,die_cut_price)
+    lab_df.loc["Pasting"]=(pasting_material,0,0,pasting_material)
+    lab_df.loc["Uv Coating"]=(UV_coating,0,0,UV_coating)
+    lab_df.loc["Foiling"]=(foiling,0,0,foiling)
+    lab_df.loc["Debossing"]=(Debosing,0,0,Debosing)
+    lab_df.loc["Embossing"]=(Embosing,0,0,Embosing)
+    lab_df.loc["Carrug Lab"]=(carrug_lab,0,0,carrug_lab)
+    lab_df.loc["Lab Total"]=(0,0,0,lab_df.iloc[:-1,3].sum())
+    lab_df.reset_index(inplace=True)
+
+
+    #######################################################
+    ctp=CTP_Plates_price(machine_rate,process_color,pantone_color,matallic_color)
+    paper_price=paper_material(W_S,L_S,gsm,print_sheet,Material,machine_rate)
+    die_making_price=Die_making_price(machine_rate)
+    foil_block=foil_block_price(Foil,f_l,f_w,machine_rate)
+    # deboss_price_Material=DebossBlock_price(W_P,L_P)   ###############  Function should be updated
+    # # emboss_price_Material=EmbossBlock_price(Emboss,W_P,L_P)   ###############  Function should be updated
+    # Carrugation_price_Material=carrugation_price_Material(stock,W_S,L_S,laminate_sheet) ### Editing
+
+    #########################################################
+    st.session_state["material_df"].set_index('index',inplace=True)
+    st.session_state["material_df"].loc["CTP Plates"]=(0,0,0,ctp)
+    st.session_state["material_df"].loc["Paper"]=(0,paper_price,0,paper_price)
+    st.session_state["material_df"].loc["Die Making"]=(0,0,0,die_making_price)
+    st.session_state["material_df"].loc["Foil Block"]=(0,0,0,foil_block)
+    # # st.session_state["material_df"].loc["DebossBlock"]=(0,0,0,deboss_price_Material)
+    # # st.session_state["material_df"].loc["EmbossBlock"]=(0,0,0,emboss_price_Material)
+    # st.session_state["material_df"].loc["Carrugation"]=(0,0,0,Carrugation_price_Material)
+    st.session_state["material_df"].loc["Material"]=(0,0,0,st.session_state["material_df"].iloc[:-1,3].sum())
+
+    st.session_state["material_df"].reset_index(inplace=True)
+    #############################################
+else:
+     print_sheet=laminate_sheet=0
+
+
+##########################################################
+col1, col2, col3 = st.columns(3)
+col1.metric("Sheet",print_sheet, laminate_sheet)
+col2.metric("Total",1.0, "")
+col3.metric("cost per Unit", 0, "")
+col1, col2, col3 = st.columns(3)
+col1.metric("Material Cost",0, "")
+col2.metric("Labour Cost",1.0, "")
+col3.metric("Mics", 0, "")
+
+col1,col2=st.columns(2)
+n_rows=13
+height = int(35.2*(n_rows+1))
+col1.header("Material Cost")
+col1.dataframe(st.session_state["material_df"], width=700, height=410,hide_index=True)
+col2.header("Labour Cost")
+col2.dataframe(lab_df, width=700, height=height,hide_index=True)
---
+++ function_file.py
@@ -0,0 +1,310 @@
+import streamlit as st
+import pandas as pd
+import math
+import os
+st.set_page_config(layout="wide")
+if "df" not in st.session_state:
+  if os.path.exists("Rate.csv"):
+      st.session_state["df"]=pd.read_csv("Rate.csv")
+  else:
+      st.session_state["df"]=pd.read_csv("rate_.csv")
+      st.session_state["df"].to_csv("Rate.csv",index=False)
+
+@st.cache_data
+def carrugation_price_Material(stock,w_s,l_s,laminate_sheet):
+    if stock=="L1":
+        return int(w_s*l_s*laminate_sheet/2400*70)
+    elif stock=="E Flute":
+        return int(w_s*l_s*laminate_sheet/2400*120)
+    elif stock=="E Flute":
+        return int(w_s*l_s*laminate_sheet/2400*120)
+    else:
+        return 0
+@st.cache_data
+def EmbossBlock_price(Emboss,w_p,l_p):
+    if Emboss=="Yes":
+        return int(w_p*l_p*38/3)
+    else:
+        return 0
+@st.cache_data
+def DebossBlock_price(w_p,l_p):
+    if (w_p>=1) and (l_p>=1):
+        return int(w_p*l_p*38/3)
+    else:
+        return 0
+@st.cache_data
+def foil_block_price(foiling,w_p,l_p,machine_rate,):
+    # print(machine_rate[foiling].iloc[0])
+    if (foiling!="None") and ((w_p>=1) and (l_p>=1)):
+        return int(w_p*l_p*machine_rate[foiling].iloc[0])
+    else:
+        return 0
+@st.cache_data
+def Die_making_price(machine):
+    return machine["Die Making"].iloc[0]
+
+@st.cache_data
+def paper_material(w_s,l_s,gms,print_sheet,material,machine,):
+    # print(material=="Bleached Card")
+    # print(machine.columns)
+    try:
+        return int(w_s*l_s*gms/15500*print_sheet/100*(machine[material].iloc[0]))
+    except:
+        return 0
+    # if gms<=350 and material=="Bleach Card":
+    #     return int(w_s*l_s*gms/15500*print_sheet/100*(400))
+    # elif gms>350 and material=="Bleach Card":
+    #     return int(w_s*l_s*gms/15500*print_sheet/100*(450))
+    # elif gms<=350 and material=="Bux Board":
+    #     return int(w_s*l_s*gms/15500*print_sheet/100*(350))
+    # elif gms>350 and material=="Bux Board":
+    #     return int(w_s*l_s*gms/15500*print_sheet/100*(425))
+    # elif gms<=350 and material=="Kraft":
+    #     return int(w_s*l_s*gms/15500*print_sheet/100*(350))
+    # elif gms>350 and material=="Kraft":
+    #     return int(w_s*l_s*gms/15500*print_sheet/100*(425))
+    # elif gms<=350 and material=="Art Card":
+    #     return int(w_s*l_s*gms/15500*print_sheet/100*(350))
+    # elif gms>350 and material=="Art Card":
+    #     return int(w_s*l_s*gms/15500*print_sheet/100*(425))
+    # else:
+    #     return 0
+
+@st.cache_data
+def CTP_Plates_price(Machine,process_color,pantone_color,matallic_color):
+    try:
+        # print(process_color,pantone_color,matallic_color)
+        return Machine["CTP"].iloc[0]*(process_color+pantone_color+matallic_color)
+    except Exception as e:
+        # print(e)
+        return 0
+
+@st.cache_data
+def corgation_price(w_s,l_s,pasting,Lamination_sheet,machine_rate):
+    try:
+        if pasting=="Single Side":
+            # print(w_s*l_s*machine_rate["carugation_pasting_rate"].iloc[0]*Lamination_sheet/2400)
+            return int(w_s*l_s*machine_rate["carugation_pasting_rate"].iloc[0]*Lamination_sheet/2400)
+        elif pasting=="Double Side":
+            # print(machine_rate["carugation_pasting_rate"].iloc[0]*2)
+            return int(2*w_s*l_s*machine_rate["carugation_pasting_rate"].iloc[0]*Lamination_sheet/2400)
+        else:
+            # print(machine_rate["carugation_pasting_rate"].iloc[0]*0)
+            return 0
+    except Exception as e:
+        return 0
+@st.cache_data
+def embosing_price(E_l,E_w,Machine,print_sheet):
+    try:
+        if (E_l>=1) and (E_w>=1):
+        # as printing rate is per 1170 and below list show us that if printing rate is 1400 and we have sheets 3350 it is below 4387 so we multply its index+1 to rate
+            sheet_list =[1170, 2310, 3300, 4387, 5360, 6405, 7500, 8500, 9532, 10529, 11500, 12500, 13553, 14595, 15742, 16680, 17723, 18765, 19808, 20850, 21893, 22935, 23978, 25020, 26167, 27105, 28252, 29294, 30233, 31275, 32318, 33360, 34403, 35485, 36488, 37634, 38677, 39719]
+            for i in sheet_list:
+                if print_sheet<=i:
+                    factor=sheet_list.index(i)+1
+                    break
+            return Machine["Embos"].iloc[0]*factor
+        else:
+            return 0
+    except:
+        return 0
+@st.cache_data
+def debosing_price(d_w,d_l,Machine,print_sheet):
+    try:
+        if (d_w>=1) and (d_l>=1):
+        # as printing rate is per 1170 and below list show us that if printing rate is 1400 and we have sheets 3350 it is below 4387 so we multply its index+1 to rate
+            sheet_list =[1170, 2310, 3300, 4387, 5360, 6405, 7500, 8500, 9532, 10529, 11500, 12500, 13553, 14595, 15742, 16680, 17723, 18765, 19808, 20850, 21893, 22935, 23978, 25020, 26167, 27105, 28252, 29294, 30233, 31275, 32318, 33360, 34403, 35485, 36488, 37634, 38677, 39719]
+            for i in sheet_list:
+                if print_sheet<=i:
+                    factor=sheet_list.index(i)+1
+                    break
+            return Machine["Debos"].iloc[0]*factor
+        else:
+            return 0
+    except:
+        return 0
+@st.cache_data
+def foil_price(f_l,f_w,Foil,laminate_sheet,machine_rate):
+    if (Foil!="None") and ((f_l>=1) and (f_w>=1)):
+        return int(f_l*f_w*laminate_sheet*machine_rate[Foil].iloc[0])
+    else:
+        return 0
+    
+def UV_price(UV_L,UV_W,print_sheet,machine_rate):
+    try:
+        if (UV_L>=1) and (UV_W>=1):
+            # print(int(UV_L*UV_W*printable_quantity*machine_rate["UV_price"][0]))
+            return int(UV_L*UV_W*print_sheet*machine_rate["UV / Spot UV"].iloc[0])
+        else:
+            return 0
+    except:
+        return 0
+@st.cache_data   
+def Pasting_Calculator(Machine,Req_Q):
+    try:
+        thresholds = [
+            1050, 2100, 3150, 4200, 5250, 6300, 7350, 8400, 9450, 10500,
+            11500, 12500, 13500, 14500, 15500, 16500, 17500, 18500, 19600,
+            20700, 21700, 22700, 23978, 25020, 26167, 27105, 28252, 29294,
+            30233, 31275, 32318, 33360, 34403, 35485, 36488, 37634, 38677, 39719
+        ]
+        for i, threshold in enumerate(thresholds, start=1):
+            if Req_Q <= threshold:
+                factor=i
+                break
+        return Machine["Pasting"].iloc[0]*i
+    except:
+        return 0
+@st.cache_data
+def Die_cut_price(print_sheet,Machine):
+    try:
+        # as printing rate is per 1170 and below list show us that if printing rate is 1400 and we have sheets 3350 it is below 4387 so we multply its index+1 to rate
+        sheet_list =[1170, 2310, 3300, 4387, 5360, 6405, 7500, 8500, 9532, 10529, 11500, 12500, 13553, 14595, 15742, 16680, 17723, 18765, 19808, 20850, 21893, 22935, 23978, 25020, 26167, 27105, 28252, 29294, 30233, 31275, 32318, 33360, 34403, 35485, 36488, 37634, 38677, 39719]
+        for i in sheet_list:
+            if print_sheet<=i:
+                factor=sheet_list.index(i)+1
+                break
+        return Machine["Die Cut"].iloc[0]*factor
+    except:
+        return 0
+@st.cache_data
+def Lamination_sheets_calculator(sheet):
+    try:
+        if sheet <= 100:
+            return math.ceil(sheet * 0.5 + sheet)
+        elif sheet <= 200:
+            return math.ceil(sheet * 0.5 + sheet)
+        elif sheet <= 300:
+            return math.ceil(sheet * 0.4 + sheet)
+        elif sheet <= 400:
+            return math.ceil(sheet * 0.2 + sheet)
+        elif sheet <= 500:
+            return math.ceil(sheet * 0.2 + sheet)
+        elif sheet <= 600:
+            return math.ceil(sheet * 0.18 + sheet)
+        elif sheet >= 650 and sheet <= 1000:
+            return math.ceil(sheet * 0.15 + sheet)
+        elif sheet >= 1000 and sheet <= 1500:
+            return math.ceil(sheet * 0.1 + sheet)
+        elif sheet >= 1500 and sheet <= 2000:
+            return math.ceil(sheet * 0.12 + sheet)
+        elif sheet >= 2000 and sheet <= 3000:
+            return math.ceil(sheet * 0.08 + sheet)
+        elif sheet >= 3000 and sheet <= 4000:
+            return math.ceil(sheet * 0.04 + sheet)
+        elif sheet >= 4000 and sheet <= 7000:
+            return math.ceil(sheet * 0.04 + sheet)
+        elif sheet > 7000:
+            return math.ceil(sheet * 0.025 + sheet)
+        else:
+            return sheet
+    except:
+        return 0
+@st.cache_data
+def Print_Sheet_calculator(sheet):
+    try:
+        if sheet <= 100:
+            return math.ceil(sheet * 1.5 + sheet)
+        elif sheet <= 200:
+            return math.ceil(sheet * 1.0 + sheet)
+        elif sheet <= 300:
+            return math.ceil(sheet * 0.65 + sheet)
+        elif sheet <= 400:
+            return math.ceil(sheet * 0.45 + sheet)
+        elif sheet <= 500:
+            return math.ceil(sheet * 0.35 + sheet)
+        elif sheet <= 600:
+            return math.ceil(sheet * 0.3 + sheet)
+        elif sheet >= 700 and sheet <= 1000:
+            return math.ceil(sheet * 0.17 + sheet)
+        elif sheet >= 1000 and sheet <= 1500:
+            return math.ceil(sheet * 0.15 + sheet)
+        elif sheet >= 1500 and sheet <= 2000:
+            return math.ceil(sheet * 0.15 + sheet)
+        elif sheet >= 2000 and sheet <= 3000:
+            return math.ceil(sheet * 0.1 + sheet)
+        elif sheet >= 3000 and sheet <= 4000:
+            return math.ceil(sheet * 0.08 + sheet)
+        elif sheet >= 4000 and sheet <= 5000:
+            return math.ceil(sheet * 0.07 + sheet)
+        elif sheet <= 6000:
+            return math.ceil(sheet * 0.05 + sheet)
+        elif sheet <= 7000:
+            return math.ceil(sheet * 0.05 + sheet)
+        elif sheet <= 8000:
+            return math.ceil(sheet * 0.05 + sheet)
+        elif sheet <= 9000:
+            return math.ceil(sheet * 0.045 + sheet)
+        elif sheet <= 10000:
+            return math.ceil(sheet * 0.0475 + sheet)
+        elif sheet > 10000:
+            return math.ceil(sheet * 0.0425 + sheet)
+    except:
+        return 0
+
+@st.cache_data
+def find_machine_size(w, l,rate_df):
+    
+    if (w <= 12.5 and l <= 18) or (l <= 12.5 and w <= 18):
+        machine = rate_df[rate_df.Machine_size == "12x17"]
+        # print("12x17")
+    elif (w <= 25 and l <= 18) or (l <= 25 and w <= 18):
+        machine = rate_df[rate_df.Machine_size == "23x17"]
+        # print("23x17")
+    elif (w <= 25 and l <= 36) or (l <= 25 and w <= 36):
+        machine = rate_df[rate_df.Machine_size == "25x36"]
+        # print("25x36")
+    elif (w <= 28 and l <= 40) or (l <= 28 and w <= 40):
+        machine = rate_df[rate_df.Machine_size == "28x40"]
+        # print("28x40")
+    elif (w <= 35 and l <= 45) or (l <= 35 and w <= 45):
+        machine = rate_df[rate_df.Machine_size == "35x45"]
+        # print("35x45")
+    elif (w <= 40 and l <= 60) or (l <= 40 and w <= 60):
+        machine = rate_df[rate_df.Machine_size == "40x56"]
+        # print("40x56")
+    else:
+        # print("No matching machine size found.")
+        machine = None
+    return machine
+    # st.dataframe(machine, hide_index=True)
+@st.cache_data
+def Printing_Calculator(Machine,cmyk,pms,met,print_sheet):
+    try:
+        # as printing rate is per 1170 and below list show us that if printing rate is 1400 and we have sheets 3350 it is below 4387 so we multply its index+1 to rate
+        sheet_list =[1170, 2310, 3300, 4387, 5360, 6405, 7500, 8500, 9532, 10529, 11500, 12500, 13553, 14595, 15742, 16680, 17723, 18765, 19808, 20850, 21893, 22935, 23978, 25020, 26167, 27105, 28252, 29294, 30233, 31275, 32318, 33360, 34403, 35485, 36488, 37634, 38677, 39719]
+        for i in sheet_list:
+            if print_sheet<=i:
+                factor=sheet_list.index(i)+1
+                break
+            # CMYK=Machine["CMYK"].iloc[0, 0]
+        return Machine["CMYK"].iloc[0]*cmyk*factor,Machine["PMS"].iloc[0]*pms*factor,Machine["Met"].iloc[0]*met*factor
+    except:
+        return 0,0,0
+@st.cache_data
+def Lamination_price_calculator(w_p,l_p,Sheet_printable,inside_rate,outside_rate,rate_df):
+    # Outside_rate=Inside_rate=0
+    try:
+        # print(rate_df[outside_rate][0])
+        Outside_rate=rate_df[outside_rate][0]
+    except:
+        Outside_rate=0
+    try:
+        Inside_rate=rate_df[inside_rate][0]
+    except:
+        # print(inside_rate)
+        Inside_rate=0
+    # if Inside_rate!="None":
+        # Inside_rate=rate_df[inside_rate][0]
+    
+    outside_lamination= int((w_p*l_p/144)*Outside_rate*Sheet_printable)
+    inside_lamination= int((w_p*l_p/144)*Inside_rate*Sheet_printable)
+    return outside_lamination,inside_lamination
+
+@st.cache_data
+def load_data(file,sheet_name):
+    # Load your data into a DataFrame (replace 'your_data.csv' with your actual data source)
+    data = pd.read_excel(file,sheet_name=sheet_name)
+    data.fillna(0,inplace=True)
+    return data
+
